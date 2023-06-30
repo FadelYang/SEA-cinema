5907fa68b00b3d1447689f78eb17e843de45e2b8
fadelanumah <fadelanumah@gmail.com>
2023-06-30 19:20:35 +0700

    feat(movie-list): Add Movie List ServiceProvider
    add movie list service provider for share same data to multiple view

---
+++ app/Providers/ViewComposers/ComposerServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+ 
+namespace App\Providers\ViewComposers;
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\ServiceProvider;
+ 
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Using Closure based composers...
+        view()->creator(['home', 'welcome'], function ($view) {
+            $movies = app(HomeController::class)->getMovieList();
+
+            $view->with('movies', $movies); 
+        });
+    }
+ 
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
