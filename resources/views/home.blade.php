afd7fa24c39c0a8acdee290e78b1bc465bc22543
fadelanumah <fadelanumah@gmail.com>
2023-06-30 20:03:19 +0700

    feat(home): Add Responsive Design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    {{-- movie list section --}}
+    <section class="container mt-5">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="row col-xl-6 mb-2">
+                    <div class="col-sm-6 imageContainer">
+                        <img src="{{ $movie->poster_url }}" alt="{{ $movie->title }} poster" class="img-fluid image">
+                        <div class="detailButton">
+                            <a class="btn btn-light mb-1" href="#">Lihat Detail</a>
+                            <a class="btn btn-dark" href="#">Beli Tiket</a>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
+                    <div class="col-sm-6 d-flex flex-column justify-content-xl-between">
+                        <div>
+                            <p class="h3 mt-2">{{ $movie->title }}</p>
+                            <p class="movieDescription">{{ $movie->description }}</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 col-xl-12 col-12">
+                                <p>Release Date: <span
+                                        class="badge bg-primary">{{ Carbon\Carbon::createFromFormat('Y-m-d', $movie->release_date)->format('d F Y') }}</span>
+                                </p>
+                                <p>Age Rating: <span class="badge bg-primary">{{ $movie->age_rating }}</span></p>
+                                <p>Ticket Price: <span class="badge bg-primary">{{ $movie->ticket_price }}</span></p>
+                            </div>
+                            <div class="col-md-6 col-xl-12 col-12 d-xl-none detailButtonResponsive">
+                                <a class="btn btn-light mb-1" href="#">Lihat Detail</a>
+                                <a class="btn btn-warning" href="#">Beli Tiket</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-    </div>
-</div>
+    </section>
 @endsection
+
+@push('css')
+    <style>
+        .movieDescription {
+            overflow: scroll;
+            display: -webkit-box;
+            -webkit-line-clamp: 6;
+            -webkit-box-orient: vertical;
+        }
+
+        .detailButton {
+            transition: .5s ease;
+            opacity: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            text-align: center;
+        }
+
+        .image {
+            opacity: 1;
+            display: block;
+            width: 100%;
+            height: auto;
+            transition: .5s ease;
+            backface-visibility: hidden;
+        }
+
+        .imageContainer {
+            position: relative
+        }
+
+        .imageContainer:hover .image {
+            opacity: 0.3;
+        }
+
+        .imageContainer:hover .detailButton {
+            opacity: 1;
+        }
+    </style>
+@endpush
