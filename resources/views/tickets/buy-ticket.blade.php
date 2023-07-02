2f707035185045880692f33e8dc26c0b4e596a68
fadelanumah <fadelanumah@gmail.com>
2023-07-02 08:48:08 +0700

    feat(ticket): Add Film Detail In Page

--- resources/views/tickets/buy-ticket.blade.php
+++ resources/views/tickets/buy-ticket.blade.php
@@ -31,9 +31,32 @@
             </div>
 
             <div class="col-sm-4 mt-sm-0 mt-5">
-                <p class="h3 text-center">Film Detail</p>
+                <div class="row">
+                    <div class="col-12">
+                        <p class="h3 text-center mb-5">Film Detail</p>
+
+                        @foreach ($movie as $movie)
+                            {{-- detail movie section --}}
+                            <p class="h5">Title : <span
+                                    class="badge bg-primary">{{ $movie->title }}</span>
+                            </p>
+                            <p class="h5">Release Date : <span
+                                    class="badge bg-primary">{{ Carbon\Carbon::createFromFormat('Y-m-d', $movie->release_date)->format('d F Y') }}</span>
+                            </p>
+                            <p class="h5">Ticket Price : <span
+                                    class="badge bg-primary">{{ $movie->ticket_price }}</span></p>
+                            <p class="h5">Age Rating : <span class="badge bg-primary">{{ $movie->age_rating }}</span>
+                        @endforeach
+                    </div>
+                    <div class="col-12 mt-5">
+                        <p class="h3 text-center">Ticket Detail</p>
+                    </div>
+                </div>
             </div>
+
         </div>
+    </div>
+    </div>
     </div>
 @endsection
 
