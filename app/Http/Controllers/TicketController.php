7e2a0df203f1caecf7725a5047794b16b5812fb1
fadelanumah <fadelanumah@gmail.com>
2023-07-03 17:23:00 +0700

    feat(buy-ticket): Add Validation Before Buy Ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Symfony\Component\Console\Input\Input;
 
 class TicketController extends Controller
 {
@@ -24,5 +26,26 @@ class TicketController extends Controller
         return view('tickets.buy-ticket', [
             'movie' => $movieItem,
         ]);
+    }
+
+    public function buyTicket(Request $request)
+    {
+        $selectedSeats = $request->input('seats'); // get all selected seats
+
+        $totalPrice = $request->ticket_price * count($selectedSeats);
+
+        if (auth()->user()->balance < $totalPrice) {
+            return Redirect::back()->with('message', "Total balance anda tidak mencukupi untuk membeli tiket (total harga tiket: $totalPrice, balance anda: " . auth()->user()->balance . ")");
+        }
+
+
+        if ($selectedSeats === null) {
+            return Redirect::back()->with('message', 'Silahkan pilih tempat duduk terlebih dahulu');
+        }
+
+        foreach ($selectedSeats as $seatNumber) {
+            
+        }
+        // dd($input['seats'], count($input['seats']));
     }
 }
