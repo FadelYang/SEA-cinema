d470bf0baf35225da5ed423838d2b23877f15cbd
fadelanumah <fadelanumah@gmail.com>
2023-07-03 17:04:13 +0700

    feat(alert-with-message): Add Timer

--- resources/views/components/alert-with-message.blade.php
+++ resources/views/components/alert-with-message.blade.php
@@ -1,5 +1,5 @@
 @if (session('message') || session('error'))
-    <div class="col-md-12">
+    <div class="col-md-12 alert-message">
         <div class="alert alert-danger">
             {{ session('message') }}
         </div>
@@ -7,9 +7,19 @@
 @endif
 
 @if (session('success') || session('error'))
-    <div class="col-md-12">
+    <div class="col-md-12 alert-message">
         <div class="alert alert-success">
             {{ session('success') }}
         </div>
     </div>
 @endif
+
+@push('js')
+    <script>
+        $("document").ready(function() {
+            setTimeout(function() {
+                $(".alert-message").fadeOut();
+            }, 10000);
+        });
+    </script>
+@endpush
