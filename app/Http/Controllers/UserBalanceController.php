342f4ae62f93d1e59110a48928f2e5aaf13f0709
fadelanumah <fadelanumah@gmail.com>
2023-07-03 14:22:08 +0700

    feat(topup): Add Top UP History

--- app/Http/Controllers/UserBalanceController.php
+++ app/Http/Controllers/UserBalanceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TopUpBalanceHistoryModel;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
@@ -29,8 +30,30 @@ class UserBalanceController extends MovieController
         $currentBalance = $user->balance;
         $updateBalance = $currentBalance + $request->balance;
 
+        // update balance user
         User::where('id', $user->id)->update(['balance' => $updateBalance]);
 
+        // store top up balance history
+        $this->storeTopUpBalanceHistory(userId: $user->id, topUpAmount: $request->balance);
+
         return Redirect()->back()->with('success', "Berhasil topup sebanyak $currentBalance. Balance anda sekarang $updateBalance");
+    }
+
+    public function storeTopUpBalanceHistory($userId, $topUpAmount)
+    {
+        TopUpBalanceHistoryModel::create([
+            'user_id' => $userId,
+            'amount' => $topUpAmount,
+        ]);
+    }
+
+    public function getTopUpBalanceHistory($userId)
+    {
+        return TopUpBalanceHistoryModel::where('user_id', $userId)->orderBy('created_at')->get();
+    }
+
+    public function getLatestTopUpBalanceHistory($userId)
+    {
+        return TopUpBalanceHistoryModel::where('user_id', $userId)->first();
     }
 }
---
+++ app/Models/TopUpBalanceHistoryModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class TopUpBalanceHistoryModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'top_up_balance_history';
+
+    protected $fillable = [
+        'user_id',
+        'amount',
+    ];
+
+    /**
+     * Get the user that owns the TopUpBalanceHistoryModel
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
