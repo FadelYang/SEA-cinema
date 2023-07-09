79b80c21f4c1917ccd8d5e0be488625a80988159
fadelanumah <fadelanumah@gmail.com>
2023-07-09 07:37:46 +0700

    fix(buy-ticket): Fix Update User Balance After Buy Ticket

--- app/Repositories/EloquentUserRepository.php
+++ app/Repositories/EloquentUserRepository.php
@@ -2,10 +2,20 @@
 
 namespace App\Repositories;
 
+use App\Models\User;
+
 class EloquentUserRepository
 {
     public function getLoginUser()
     {
         return auth()->user();
+    }
+
+    public function updateBalanceUserAfterBuyTicket($totalPrice)
+    {
+        $user = $this->getLoginUser();
+
+        User::where('id', $user->id)
+            ->update(['balance' => ($user->balance - $totalPrice)]);
     }
 }
