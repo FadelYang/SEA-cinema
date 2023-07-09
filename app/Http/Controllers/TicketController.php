24fb2ef5c89e2dc44f4b754f34c867d18d1a8687
fadelanumah <fadelanumah@gmail.com>
2023-07-09 13:12:04 +0700

    refactor(ticket-transaction): Implement SRP

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -6,21 +6,21 @@ use App\Enum\TicketStatusEnum;
 use App\Models\TicketTransactionModel;
 use App\Models\User;
 use App\Services\ticketTransactionService;
-use Carbon\Carbon;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Validation\Rule;
-use Symfony\Component\Console\Input\Input;
-use Hidehalo\Nanoid\Client;
-use Hidehalo\Nanoid\GeneratorInterface;
 
 class TicketController extends Controller
 {
     private $ticketTransactionService;
+    private $userService;
 
-    public function __construct(ticketTransactionService $ticketTransactionService)
-    {
+    public function __construct(
+        ticketTransactionService $ticketTransactionService,
+        UserService $userService
+    ) {
         $this->ticketTransactionService = $ticketTransactionService;
+        $this->userService = $userService;
     }
 
     public function getBuyTicketPage($movieTitle)
@@ -28,9 +28,9 @@ class TicketController extends Controller
         $buyTicketData =  $this->ticketTransactionService->getBuyTicketPage($movieTitle);
 
         $isUserAgeUnderMovieRating = $this->ticketTransactionService->isUserAgeUnderMovieRating(
-                $buyTicketData['movieItem'],
-                $buyTicketData['userAge']
-            );
+            $buyTicketData['movieItem'],
+            $buyTicketData['userAge']
+        );
 
         if ($isUserAgeUnderMovieRating) {
             return Redirect::back()->with('message', 'Umur anda tidak mencukupi untuk film yang anda pilih');
@@ -57,13 +57,16 @@ class TicketController extends Controller
             if ($buyTicket['selectedSeats'] === null) {
                 return Redirect::back()->with('message', 'Silahkan pilih tempat duduk terlebih dahulu');
             }
-            
+
             if ($buyTicket['totalUserTicketPerFilm'] > 6) {
                 return Redirect::back()->with('message', "Anda sudah mencapai batas jumlah tiket yang dapat dibeli");
             }
 
             if ($buyTicket['userBalance'] < $buyTicket['totalTicketPrice']) {
-                return Redirect::back()->with('message', "Total balance anda tidak mencukupi untuk membeli tiket (total harga tiket: $totalPrice, balance anda: $userBalance)");
+                return Redirect::back()->with(
+                    'message',
+                    "Total balance anda tidak mencukupi untuk membeli tiket (total harga tiket: " . $buyTicket['totalTicketPrice'] . " , balance anda: " .  $buyTicket['user']->balance . ")"
+                );
             }
 
             $this->ticketTransactionService->storeBuyTicketData(
@@ -74,60 +77,55 @@ class TicketController extends Controller
 
             return Redirect::back()->with('success', "Berhasil membeli tiket");
         } catch (\Throwable $th) {
-            dd($th);
             return Redirect::back()->with('message', "Ada kesalahan, pastika anda sudah memilih tempat duduk dengan benar");
         }
     }
 
-    public function getTicketDetail($user = null, $ticketXId)
+    public function getTicketDetail($user, $ticketXId)
     {
-        $user = auth()->user()->id;
-        $ticketItem = TicketTransactionModel::where('xid', $ticketXId)->first();
+        $ticketDetail = $this->ticketTransactionService->getTicketDetail($ticketXId);
 
-        return view('tickets.detail-ticket', ['user' => $user, 'ticketItem' => $ticketItem]);
+        return view(
+            'tickets.detail-ticket',
+            [
+                'user' => $ticketDetail['user'],
+                'ticketItem' => $ticketDetail['ticketItem']
+            ]
+        );
     }
 
     public function getIndexTicketTransactionHistoryPage()
     {
-        $user = auth()->user();
-        $userTicketTransactionHistory = $this->getAllTicketTransaction($user->id);
+        $user = $this->userService->getLoginUser();
+        $userTicketTransactionHistory = $this->ticketTransactionService->getAllTicketTransaction($user->id);
 
         return view('tickets.index-history', [
-            'ticketTransactionHistory' => $userTicketTransactionHistory,
+            'ticketTransactionHistory' => $userTicketTransactionHistory['ticketData'],
         ]);
     }
 
-    public function cancelBuyTicket($user = null, $ticketXId)
+    public function cancelBuyTicket($user, $ticketXId)
     {
         try {
-            $user = auth()->user();
-            $ticketItem = TicketTransactionModel::where([
-                ['user_id', $user->id],
-                ['xid', $ticketXId],
-            ])->first();
+            $user = $this->userService->getLoginUser();
 
-            if ($ticketItem->status === TicketStatusEnum::CANCELED->value) {
+            $ticketItem = $this->ticketTransactionService->getTicketDetail($ticketXId);
+
+            if ($ticketItem['ticketItem']->status === TicketStatusEnum::CANCELED->value) {
                 return Redirect()->back()->with('message', 'Tiket anda sudah dibatalkan');
             }
 
-            $ticketItem->update(['status' => TicketStatusEnum::CANCELED]);
-
-            // update user balance when canceled process success and create new top up history
-            (new UserBalanceController)
-                ->storeTopUpBalanceHistory(
-                    userId: $user->id,
-                    topUpAmount: $ticketItem->ticket_price,
-                    topUpNotes: "Ticket Refund"
+            $this->ticketTransactionService
+                ->cancelBuyTicket(
+                    user: $user,
+                    ticketXId: $ticketItem['ticketItem']->xid,
+                    ticketPrice: $ticketItem['ticketItem']->ticket_price
                 );
-
-            // update balance user
-            User::where('id', $user->id)->update(['balance' => $user->balance + $ticketItem->ticket_price]);
 
             return Redirect()->back()->with('success', 'Berhasil membatalkan tiket, saldo anda sudah diupdate sesuai jumlah tiket yang direfund');
         } catch (\Throwable $th) {
-            return Redirect()->back()->with('error', 'Gagal membatalkan tiket, silahkan coba lagi');
+            dd($th);
+            return Redirect()->back()->with('message', 'Gagal membatalkan tiket, silahkan coba lagi');
         }
-      
-
     }
 }
