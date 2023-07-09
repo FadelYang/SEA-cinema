284a182d76f642b654eed1339454e4f68bfa2aeb
fadelanumah <fadelanumah@gmail.com>
2023-07-09 13:29:34 +0700

    fix(ticket-cancel): Fix Store History Balance After Cancel Ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -124,7 +124,6 @@ class TicketController extends Controller
 
             return Redirect()->back()->with('success', 'Berhasil membatalkan tiket, saldo anda sudah diupdate sesuai jumlah tiket yang direfund');
         } catch (\Throwable $th) {
-            dd($th);
             return Redirect()->back()->with('message', 'Gagal membatalkan tiket, silahkan coba lagi');
         }
     }
